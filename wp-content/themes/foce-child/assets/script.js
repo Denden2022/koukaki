0c31d6f81bc847b19aaa92e87a82e16e3e56dab7
Denise Fontanier <deniseden.c@gmail.com>
2024-01-02 15:49:43 +0100

    effet fade in

---
+++ wp-content/themes/foce-child/assets/script.js
@@ -0,0 +1,4 @@
+let TitleStudio = document.querySelector('#studio h2');
+TitleStudio.className.add = ('hidden');
+// Vérifiez la classe dans la console
+console.log(TitleStudio.classList);
---
+++ wp-content/themes/foce-child/template-parts/content-oscars.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template part for displaying nomination's oscars
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Fleurs_d\'oranger_&_Chats_errants
+ */
+
+?>
+
+<section id="oscars-id" class="oscars-bloc">
+    <div class="oscars-orange">
+        <img class="oscars-background" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/orange_oscars.png'; ?> " alt="fond couleur Oscars">
+        <p class="oscars-text">Fleurs d'oranger & chats errants est nominé aux Oscars Short Film Animated de 2022</p>
+    </div> 
+        <img class="oscars-nomination" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/oscars.svg'; ?> " alt="titre Oscars">
+</section>
