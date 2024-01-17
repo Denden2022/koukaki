4471dfb8ab1f62134a8b583b0f7720adb07a54d1
Denise Fontanier <deniseden.c@gmail.com>
2024-01-17 11:18:07 +0100

    modification media queries

--- wp-content/themes/foce-child/js/script.js
+++ wp-content/themes/foce-child/js/script.js
@@ -1,75 +1,10 @@
 /**
  * Effet parallax sur le titre de la vidéo et effet de flottement
- * Au scroll, le logo descend et s'arrête en bas de la vidéo
  */
-/*function LogoAnimation() {
-  const logo = document.querySelector('.parallax-logo');
-  const stopAnimationAt = 300; // Remplacez 500 par la position de défilement à laquelle vous souhaitez arrêter l'animation
-
-  const handleScroll = () => {
-    const scrollTop = window.scrollY;
-    const parallaxValue = scrollTop * 0.5;
-
-    // Si l'élément est dans la vue et la position de défilement n'a pas dépassé le point d'arrêt
-    if (logo.classList.contains('in-view') && scrollTop <= stopAnimationAt) {
-      logo.style.transform = `translateY(${parallaxValue}px)`;
-      logo.style.animation = 'floatAnimation 2s ease-in-out infinite';
-    } else {
-      logo.style.animation = 'none'; // Arrêter l'effet de flottement quand l'animation de défilement est désactivée
-    }
-  };
-
-  const seeLogo = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        logo.classList.add('in-view');
-        window.addEventListener('scroll', handleScroll);
-      } else {
-        logo.classList.remove('in-view');
-        window.removeEventListener('scroll', handleScroll);
-      }
-    });
-  });
-
-  seeLogo.observe(document.querySelector('#logo'));
-}
-
-LogoAnimation();*/
-
-/*function LogoAnimation() {
-  const logo = document.querySelector('.parallax-logo');
-
-  const handleScroll = () => {
-    const scrollTop = window.scrollY;
-    const parallaxValue = scrollTop * 0.5;
-
-    // Si l'élément est dans la vue, appliquez le déplacement vers le bas
-    if (logo.classList.contains('in-view')) {
-      logo.style.transform = `translateY(${parallaxValue}px)`;
-    } return
-  };
-  
-  const seeLogo = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        logo.classList.add('in-view');
-        window.addEventListener('scroll', handleScroll);
-      } else {
-        logo.classList.remove('in-view');
-        window.removeEventListener('scroll', handleScroll);
-      }
-    });
-  });
-
-  seeLogo.observe(document.querySelector('#logo'));
-}
-
-LogoAnimation();*/
-
-/*****animation de descente du Logo au scroll*/
+/*****animation de descente du Logo au scroll et qui s'arrête à la bordure de la vidéo*/
 function LogoAnimationDown() {
   const logo = document.querySelector('.parallax-logo');
-  const stopAnimationAt = 400; // Remplacez 500 par la position de défilement à laquelle vous souhaitez arrêter l'animation
+  const stopAnimationAt = 400; // Position de l'animation qui s'arrête à un point précis
 
   const handleScroll = () => {
     const scrollTop = window.scrollY;
